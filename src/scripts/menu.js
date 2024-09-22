87dc0c8d1a9626a7f48f3a8d8ea04959af0ae594
Nik Hurwood <nik@dreamleaf.co.uk>
2024-09-22 23:03:26 +0100

    added transitions and refactored blog collection

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,6 @@
 // Toggle the menu on and off
+document.addEventListener('astro:page-load', () => {
     document.querySelector('.hamburger').addEventListener('click', () => {
       document.querySelector('.nav-links').classList.toggle('expanded');
     });
+});
